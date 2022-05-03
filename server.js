a483e0a80bad23e3cd598f4b1fd0fc7e6f1fb1a2
daniela <danielao@grupodyd.com>
2022-05-03 09:48:17 -0500

    progress

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const express = require('express'); //Line 1
 const app = express(); //Line 2
 const port = process.env.PORT || 3001; //Line 3
-
+const cors = require('cors');
 const axios = require("axios");
+const corsOptions = {
+  origin: '*',
+  credentials: false
+}
+app.use(cors(corsOptions));
+
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
+
 // create a GET route
-app.get('/express_backend', (req, res) => { //Line 9
+app.get('/express_backend', (req, res, next) => { //Line 9
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
 });
 
 
-app.get('/search', async (req, res) => { //Line 9
+app.get('/search', async (req, res, next) => { //Line 9
 
-  await axios.get("https://api.mercadolibre.com/sites/MLA/search?q=​:query").then(resp => {
+  await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=:${req.query.keyword}&limit=4`).then(resp => {
 
     console.log(resp.data);
-  });
 
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
+    res.send({ success: resp.data }); //Line 10
+
+  });
 });
